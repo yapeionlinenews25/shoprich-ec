062caf3e8c9bb59d55719c7605bf55c9f870e994
yapeionlinenews25 <richesbosses@gmail.com>
2026-05-14 01:31:12 +0000

    Update account.tsx

--- src/routes/account.tsx
+++ src/routes/account.tsx
@@ -36,7 +36,7 @@ function Account() {
           throw pErr;
         }
         // near top of Account() body (right after const navigate)
-console.log("[Account] loaded, commit:", process.env?.REACT_APP_COMMIT_SHA ?? "unknown");
+        console.log("[Account] loaded, commit:", process.env?.REACT_APP_COMMIT_SHA ?? "unknown");
         
         setProfile(p || {
           user_id: user.id,
